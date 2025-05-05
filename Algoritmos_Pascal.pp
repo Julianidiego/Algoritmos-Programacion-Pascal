fa4e41168114829673ca9885c94f7e49a7a28277
Julianidiego <Julianidiego96@gmail.com>
2025-05-05 02:10:10 -0300

    Add initial Pascal program for learning purposes

---
+++ Algoritmos_Pascal.pp
@@ -0,0 +1,14 @@
+program HolaMundo;
+begin
+    writeln('Hola, mundo, este es mi primer programa en Pascal');
+    writeln('Este es un programa de ejemplo para mostrar la sintaxis de Pascal');
+    writeln('Pascal es un lenguaje de programación estructurado y de alto nivel');
+    writeln('Es utilizado principalmente en la enseñanza de la programación');
+    writeln('y en el desarrollo de software para sistemas embebidos');
+    writeln('Este programa imprime un mensaje en la consola');
+    writeln('y luego termina su ejecución');
+    writeln('Espero que este ejemplo te haya sido útil');
+    writeln('Recuerda que la programación es una habilidad que se aprende con la práctica');
+    writeln('No dudes en experimentar con el código y hacer tus propias modificaciones');
+    writeln('¡Buena suerte en tu aprendizaje de Pascal!');
+end.
