342aff2140d3fec81a943c2f5aebcc9d8f77b072
Julianidiego <Julianidiego96@gmail.com>
2025-05-05 15:10:20 -0300

    Prueba_01

---
+++ ALgoritmos_Python.py
@@ -0,0 +1,21 @@
+print("Hola Mundo")
+print("No recuerdo como se usa esto.")
+variable1 = 5
+if variable1 == 5:
+    print("La variable es 5")
+else:
+    print("La variable no es 5")
+print("Fin del programa")
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
+def multiplicacion(a, b):
+    return a * b
+def division(a, b):
+    if b == 0:
+        return "No se puede dividir entre cero"
+    else:
+        return a / b
+def potencia(a, b):
+    return a ** b
